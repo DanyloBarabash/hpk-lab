f302357f6d487fc9d138193b2452693b9e4482bc
danylo.barabash <danylo.barabash@newstream.ai>
2025-11-17 23:37:16 +0200

    feat: add new module for cache and add cache support for external api

--- src/external_api/service.py
+++ src/external_api/service.py
@@ -1,5 +1,9 @@
+import httpx
+import asyncio
 import requests
+from src.cache.service import cache_get, cache_set
 from src.external_api.models import CatFactModel, CatImageModel, CatCombinedModel
+from src.settings import settings
 
 
 class CatService:
@@ -13,9 +17,21 @@ class CatService:
         Fetch a random cat fact from catfact.ninja API.
         :return: CatFactModel with fact text and its length.
         """
+        cache_key = "cache:external:cat_fact"
+
+        # get from cache
+        cached = asyncio.run(cache_get(cache_key))
+        if cached:
+            return CatFactModel(**cached)
+
+        # fetch from API
         response = requests.get(self.fact_url, timeout=10)
         response.raise_for_status()
         data = response.json()
+
+        # save to cache
+        asyncio.run(cache_set(cache_key, data, settings.redis_TTL))
+
         return CatFactModel(**data)
 
     def get_cat_image(self) -> CatImageModel:
@@ -23,11 +39,22 @@ class CatService:
         Fetch a random cat image from thecatapi.com API.
         :return: CatImageModel with image URL.
         """
+        cache_key = "cache:external:cat_image"
+
+        # get from cache
+        cached = asyncio.run(cache_get(cache_key))
+        if cached:
+            return CatImageModel(**cached)
+
+        # fetch from API
         response = requests.get(self.image_url, timeout=10)
         response.raise_for_status()
         data = response.json()
-        return CatImageModel(url=data[0]["url"])
 
+        # save to cache
+        asyncio.run(cache_set(cache_key, data, settings.redis_TTL))
+
+        return CatImageModel(url=data[0]["url"])
     def get_cat_info(self) -> CatCombinedModel:
         """
         Combine cat fact and image into a single model.
